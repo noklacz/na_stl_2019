f79a24aeb0184f6efaaed5c7225d7077d0d7deeb
pawluch <krzysztof.pawluch@nsn.com>
2019-03-06 14:17:23 +0100

    bubblesort on list

--- test/ut/iterators/bubbleSort.cpp
+++ test/ut/iterators/bubbleSort.cpp
@@ -4,38 +4,55 @@
 
 #include <gtest/gtest.h>
 #include <vector>
-
+#include <list>
 namespace iterators
 {
-template <typename It>
-void bubble_sort(It begin, It end)
-{
-    if (begin == end)
-        return;
-
-    auto actual = begin;
-    auto next = std::next(begin);
-    bool changed = true;
-    while(changed)
+    template <typename It>
+    void bubble_sort(It begin, It end)
     {
-        changed = false;
-        while(next != end)
+        if (begin == end)
+            return;
+
+        auto actual = begin;
+        auto next = std::next(begin);
+        bool changed = true;
+        while(changed)
         {
-            if (*actual > *next)
+            changed = false;
+            while(next != end)
             {
-                changed = true;
-                std::swap(*actual, *next);
+                if (*actual > *next)
+                {
+                    changed = true;
+                    std::swap(*actual, *next);
+                }
+                actual = std::next(actual);
+                next = std::next(next);
             }
-            actual = std::next(actual);
-            next = std::next(next);
+            actual = begin;
+            next = std::next(actual);
         }
-        actual = begin;
-        next = std::next(actual);
     }
 }
-}
+
 TEST(bubble_sort, should_not_crash_with_empty_data)
 {
     std::vector<int> v{};
     iterators::bubble_sort(v.begin(), v.end());
+}
+
+TEST(bubble_sort, should_sort)
+{
+    std::vector<int> v{1,3,5,6,3,3,0,-1,4};
+    iterators::bubble_sort(v.begin(), v.end());
+    std::vector<int> expected{-1,0,1,3,3,3,4,5,6};
+    EXPECT_EQ(expected,v);
+}
+
+TEST(bubble_sort, should_sort_on_list)
+{
+    std::list<int> v{1,3,5,6,3,3,0,-1,4};
+    iterators::bubble_sort(v.begin(), v.end());
+    std::list<int> expected{-1,0,1,3,3,3,4,5,6};
+    EXPECT_EQ(expected,v);
 }
